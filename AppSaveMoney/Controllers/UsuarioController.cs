98004bd7bed39c380bbc1e93da287f831ceca7c2
SUITE <SUITE@SUITEBG102-SERV>
2025-04-15 09:07:41 -0500

    Registro de usuario

---
+++ AppSaveMoney/Controllers/UsuarioController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppSaveMoney.Controllers {
+    public class UsuarioController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
